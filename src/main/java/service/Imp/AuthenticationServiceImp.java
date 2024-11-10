17eed51fbb2fa2a3ee461a92d9b178d41f3154e1
Anna Schwarz <Aousheh.y23@gmail.com>
2024-11-10 08:48:58 +0330

    add authentication service methods

--- src/main/java/service/Imp/AuthenticationServiceImp.java
+++ src/main/java/service/Imp/AuthenticationServiceImp.java
@@ -3,31 +3,32 @@ package service.Imp;
 import entities.User;
 import entities.enums.Role;
 import repository.Imp.UserRepositoryImp;
+import service.AuthenticationService;
 
-public class AuthenticationServiceImp {
+public class AuthenticationServiceImp implements AuthenticationService {
     static UserRepositoryImp userRepositoryImp = new UserRepositoryImp();
     private static User loggedInUser;
-
+@Override
     public void setLoggedUser(User user) {
         if (loggedInUser == null) {
             loggedInUser = user;
         }
     }
-
+@Override
     public void logout() {
         if (loggedInUser != null) {
             loggedInUser = null;
         }
     }
-
+@Override
     public boolean checkRole(Role role, User user) {
         return user.getRole().equals(role);
     }
-
+@Override
     public User getLoggedUser() {
         return loggedInUser;
     }
-
+@Override
     public boolean isUserNameNew(String username, Role role) {
         for (User checkingUser : userRepositoryImp.all()) {
             if (checkingUser.getUsername().equals(username)) {
@@ -38,5 +39,4 @@ public class AuthenticationServiceImp {
         }
         return true;
     }
-
 }
